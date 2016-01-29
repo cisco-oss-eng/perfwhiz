cpu/ctx switch: Use differemt fill color for matching first 3 letters fix switch color bug coremap: use gradient blues and reds

perfwhiz/perfmap_kvm_exit_types.py
@@ -20,6 +20,8 @@
 import pandas
 from pandas import DataFrame
 from perfmap_common import output_svg_html
+from perfmap_common import get_time_span_usec
+
 import itertools
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -209,8 +211,6 @@ def show_exit_type_count(df_all_exits, df_last_exits):
     res.sort_values('total count', inplace=True, ascending=False)
     print res
 '''
-
-
 def filter_df_core(df, task_re, remove_cpu=False):
     # remove unneeded columns
     df = df.drop(['next_pid', 'pid', 'usecs', 'next_comm'], axis=1)
@@ -242,13 +242,15 @@ def get_cpu_sw_map(dfds, cap_time_usec, task_re):
         if dfd.multiplier > 1.0:
             df['duration'] = (df['duration'] * dfd.multiplier).astype(int)
         df['percent'] = ((df['duration'] * 100 * 10) // cap_time_usec) / 10
-        df['task_name'] = df['task_name'] + '.' + dfd.short_name
+        if len(dfds) > 1:
+            df['task_name'] = df['task_name'] + '.' + dfd.short_name
         df_list.append(df)
 
         # count number of rows with same task and cpu
         dfsw = DataFrame(gb.size())
         dfsw.reset_index(inplace=True)
         dfsw.rename(columns={0: 'count'}, inplace=True)
+
         if dfd.multiplier > 1.0:
             dfsw['count'] = (dfsw['count'] * dfd.multiplier).astype(int)
         dfsw_list.append(dfsw)
@@ -267,26 +269,95 @@ def get_cpu_sw_map(dfds, cap_time_usec, task_re):
     # is a list [percent, count]
     return df.set_index('task_name').T.to_dict('list')
 
-# [ { run:"run1", stats: [ { task:"vnf1", counts:[[1,10.5,231], [4,21.8,415]]},
-#                          { task:"vnf2", counts:[[31,45.1,152]]} ]
-#   },
-# ]
+
 def get_coremaps(dfds, cap_time_usec, task_re):
+    '''
     coremaps =  [
         { "run":"run1", "coremap": [
-                                     { "task":"vnf1", "counts":[[1,10.5,231], [4,21.8,415]]},
-                                     { "task":"vnf2", "counts":[[31,45.1,152]]},
-            { "task":"all tasks/core", "counts":[[1,10.5,231], [4,21.8,415],[31,45.1,152]]}
+                                     { "task":"vnf1", "counts":[[0,5.5,231], [1,15.5,231], [2,25.5,21],
+                                                                [3,35.5,231],[4,45.8,415], [5,55.5,231],
+                                                                [6,65,231], [7,75.5,231],[8,85.5,231],
+                                                                [9,95.5,231]]},
+                                     { "task":"vnf2", "counts":[[2,70.5,91], [31,45.1,152]]},
+            { "task":"all tasks", "counts":[[1,10.5,231], [2,30.5,21], [4,21.8,415],[31,45.1,152]]}
                                       ]
         },
         { "run":"run2", "coremap": [
                                      { "task":"vnf1", "counts":[[4,40.5,51], [7,0.8,215]]},
                                      { "task":"vnf2", "counts":[[30,35.1,62]]},
-            { "task":"all tasks/core", "counts":[[4,40.5,51], [7,0.8,215],[30,35.1,62]]}
+            { "task":"all tasks", "counts":[[4,40.5,51], [7,0.8,215],[30,35.1,62]]}
                                       ]
         }
     ]
+    '''
+
+    dfd = dfds[0]
+    df = dfd.df
+    time_span_usec = get_time_span_usec(df)
+
+    # remove unneeded columns
+    df = filter_df_core(df, task_re)
+
+    # at this point we have a df that looks like this:
+    #         task_name  cpu  duration
+    # 0     ASA.1.vcpu0    8      7954
+    # 1     ASA.1.vcpu0    9      5475
+    # 2     ASA.1.vcpu0    9      1000
+    # 3     ASA.1.vcpu0   11     12391
+    # 4     ASA.1.vcpu0   12     21025
+    # 5     ASA.1.vcpu0   13      6447
+    # etc...
+    if len(df) == 0:
+        print
+        print 'No selection matching "%s"' % (task_re)
+        return None
+    gb = df.groupby(['task_name', 'cpu'], as_index=False)
+
+    # because we only show percentages, there is no need to apply the multiplier
+    # add duration values
+    df = gb.aggregate(np.sum)
+    #         task_name  cpu  duration
+    # 0     ASA.1.vcpu0    8      7954
+    # 1     ASA.1.vcpu0    9      6475
+    # 2     ASA.1.vcpu0   11     12391
+
+    # Add a % column
+    df['percent'] = ((df['duration'] * 100 * 10) // time_span_usec) / 10
+
+    # many core-pinned system tasks have a duration of 0 (swapper, watchdog...)
+    df.fillna(100, inplace=True)
+    df.drop(['duration'], axis=1, inplace=True)
+
+    # count number of rows with same task and cpu
+    dfsw = DataFrame(gb.size())
+    dfsw.reset_index(inplace=True)
+    dfsw.rename(columns={0: 'count'}, inplace=True)
+    # adjust context switch count if the requested cap time is > time_span
+    if dfd.multiplier > 1.0:
+        dfsw['count'] = (dfsw['count'].astype(int) * dfd.multiplier).astype(int)
+    min_count = dfsw['count'].min()
+    max_count = dfsw['count'].max()
+    #       task_name  cpu  count
+    # 0  ASA.01.vcpu0    8   9853
+    # 1  ASA.01.vcpu0    9    348
+    # 1  ASA.01.vcpu0   11    619
+
+    # Merge the 2 df using the task_name/cpu as the joining key
+    dfm = pandas.merge(df, dfsw, how="left", on=['task_name', 'cpu'])
+    cml = []
+    gb = dfm.groupby('task_name')
+    task_list = gb.groups.keys()
+    for task in task_list:
+        counts = []
+        dfg = gb.get_group(task)
+        for index, row in dfg.iterrows():
+            counts.append([row['cpu'], row['percent'], row['count']])
+        cml.append({"task": task, "counts": counts})
+
+    coremap = {"run": dfd.short_name, "coremap":cml}
+    coremaps = [coremap]
     return coremaps
+
 
 def show_kvm_exit_types(dfds, cap_time_usec, task_re, label):
 
@@ -363,6 +434,7 @@ def show_kvm_exit_types(dfds, cap_time_usec, task_re, label):
         except KeyError:
             # this task has no context switch no cpu, so likely is
             # using up all the cpu
+            print 'keyerror:'+task_name
             cpu = 100
             sw = 1
         task_list.append({'name': task_name,